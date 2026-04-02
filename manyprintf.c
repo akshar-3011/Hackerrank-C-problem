36882003d97ae81336fd5a8db15e1e12cb008cc3
Akshar Dhakad <axardhakad99148@gmail.com>
2026-04-02 23:39:37 +0530

    Add more practice problems

---
+++ manyprintf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+    printf("I am learning C.\n");
+    printf("And it is awesome!");
+    printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+    return 0;
+}
