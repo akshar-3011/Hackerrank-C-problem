115fa442fe3764741396107c1dc29df234124d55
Akshar Dhakad <axardhakad99148@gmail.com>
2026-04-02 23:40:13 +0530

    Add sizeof function example

---
+++ sizeof(fn).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{
+int myInt;
+float myFloat;
+double myDouble;
+char myChar;
+
+printf("%zu\n", sizeof(myInt));
+printf("%zu\n", sizeof(myFloat));
+printf("%zu\n", sizeof(myDouble));
+printf("%zu\n", sizeof(myChar));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of short int: %zu bytes\n", sizeof(short int));
+printf("Size of unsigned int: %zu bytes\n", sizeof(unsigned int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of long long int: %zu bytes\n", sizeof(long long int));
+printf("Size of unsigned long long int: %zu bytes\n", sizeof(unsigned long long int));
+printf("Size of long double: %zu bytes\n", sizeof(long double));
+return 0;
+}
